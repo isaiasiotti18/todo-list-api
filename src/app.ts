a7f7b49aeed7e5b0511f13dc609ac5455c4028e4
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2026-01-19 19:49:31 -0300

    initial config

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { type Request, type Response, type NextFunction } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(morgan("tiny"));
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World");
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send(error.message);
+});
+
+export default app;
